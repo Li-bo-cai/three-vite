ec306290ab13694cc4ccd4bc4523d68f276e42bb
libocai <1519497988@qq.com>
2022-04-21 18:55:14 +0800

    1234

--- src/views/four.vue
+++ src/views/four.vue
@@ -1,61 +1,65 @@
 <template>
-    <div id="scene-four"></div>
+  <div id="scene-four"></div>
 </template>
 
 <script setup>
-import { nextTick } from 'vue';
-import * as THREE from 'three'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
+import { nextTick, onMounted } from "vue";
+import * as THREE from "three";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 // 导入控制器
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-let scene
-let camera
-let renderer
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+let scene;
+let camera;
+let renderer;
 
-nextTick(() => {
-    init()
-    anmate()
-})
+onMounted(() => {
+  init();
+  anmate();
+});
 
 function init() {
-    initScene()
-    initCamera()
-    initRanderer()
-    initControls()
+  initScene();
+  initCamera();
+  initRanderer();
+  initControls();
 }
 // 创建场景
 function initScene() {
-    scene = new THREE.Scene()
-    setEnvMap('000')
+  scene = new THREE.Scene();
+  setEnvMap();
 }
 function initCamera() {
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 200)
-    camera.position.set(300, 0, 1000);
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    200
+  );
+  camera.position.set(-1, 8, 0.6, 2.7);
 }
 function initRanderer() {
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    // 设置屏幕像素比
-    renderer.setPixelRatio(window.devicePixelRatio)
-    // 渲染尺寸大小
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.querySelector('#scene-four').appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  // 设置屏幕像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // 渲染尺寸大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.querySelector("#scene-four").appendChild(renderer.domElement);
 }
-function setEnvMap(hdr) {
-    new RGBELoader().setPath('./').load(hdr + '.hdr', (texture) => {
-        console.log(texture);
-        texture.mapping = THREE.EquirectangularReflectionMapping;
-        scene.background = texture;
-        scene.environment = texture
-    })
+function setEnvMap() {
+  new RGBELoader().setPath("./").load("000.hdr", (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+  });
 }
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 function anmate() {
-    renderer.setAnimationLoop(render())
+  renderer.setAnimationLoop(render.bind(this));
 }
 function initControls() {
-    new OrbitControls(camera, renderer.domElement)
+  let ac = new OrbitControls(camera, renderer.domElement);
 }
 </script>
 
