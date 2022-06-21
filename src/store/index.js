59d9d10132d100dffd5862af2d6ec37a1657060f
libocai <1519497988@qq.com>
2022-06-21 17:58:48 +0800

    更改pinna

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ const appStore = {}
 
 const modulesFiles = import.meta.globEager("./modules/*.js")
 
-export const registerStore = () => {
-    Object.keys(modulesFiles).map((modulePath, index) => {
-        const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1').substring(8)
-        const value = modulesFiles[modulePath]
-        appStore[moduleName] = value.default()
-    })
-}
+Object.keys(modulesFiles).map((modulePath, index) => {
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1').substring(8)
+    const value = modulesFiles[modulePath]
+    appStore[moduleName] = value.default()
+})
 
 export default appStore
